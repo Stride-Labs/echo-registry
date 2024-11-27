2eeb47f102ce69a7f2ceed3127d3a13b7713b475
Kier Borromeo <seraphipod@gmail.com>
2024-11-27 20:28:06 +0800

    fixes

--- api/echos.ts
+++ api/echos.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import { echos } from "../echos";
+import { echos } from "../src/echos";
 
 export default function (_request: VercelRequest, response: VercelResponse) {
   response.json({ echos });
